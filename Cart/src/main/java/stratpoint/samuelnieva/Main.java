fff0f8aaec2c98b95ddec82d42ee341aa742b750
Sam <sam101567@gmail.com>
2024-06-24 14:07:45 +0800

    Mini-project 3: Updated logback.xml
    - STDOUT logs levels DEBUG and above now, while FILE logs all levels

--- Cart/src/main/java/stratpoint/samuelnieva/Main.java
+++ Cart/src/main/java/stratpoint/samuelnieva/Main.java
@@ -3,12 +3,16 @@ package stratpoint.samuelnieva;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
 
-
-
-        logger.info("Test");
+        List<Product> testProducts = new ArrayList<>();
+        Catalog testCatalog = new Catalog(testProducts);
+        
+        testCatalog.addProduct(new Product(100, "Mentos 100 Pack", "Food", "Mentos"));
     }
 }

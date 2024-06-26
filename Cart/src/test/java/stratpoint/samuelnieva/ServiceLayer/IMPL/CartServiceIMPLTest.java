54582e35d8be9bf14a56fa7430d321502e63cfdf
Sam <sam101567@gmail.com>
2024-06-26 15:47:57 +0800

    Mini-project 3 Update: Added test cases for CartServiceIMPL

--- Cart/src/test/java/stratpoint/samuelnieva/ServiceLayer/IMPL/CartServiceIMPLTest.java
+++ Cart/src/test/java/stratpoint/samuelnieva/ServiceLayer/IMPL/CartServiceIMPLTest.java
@@ -1,23 +1,53 @@
 package stratpoint.samuelnieva.ServiceLayer.IMPL;
 
 import org.junit.jupiter.api.Test;
+import stratpoint.samuelnieva.EntityLayer.ProductEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class CartServiceIMPLTest {
 
     @Test
     void addProduct() {
+        CartServiceIMPL testService = new CartServiceIMPL();
+
+        testService.addProduct(new ProductEntity(1000, "Test Product", "Food", "Brand"));
+
+        List<ProductEntity> testList = new ArrayList<>();
+        testList.add(new ProductEntity(1000, "Test Product", "Food", "Brand"));
+
+        assertEquals(testService.getCartSize(), 1);
     }
 
     @Test
     void delProduct() {
+        CartServiceIMPL testService = new CartServiceIMPL();
+
+        testService.addProduct(new ProductEntity(1000, "Test Product", "Food", "Brand"));
+
+        testService.delProduct(1);
+
+        assertEquals(testService.getCartSize(), 0);
+        assertEquals(testService.getTotalPrice(), 0);
     }
 
     @Test
     void getTotalPrice() {
+        CartServiceIMPL testService = new CartServiceIMPL();
+
+        testService.addProduct(new ProductEntity(1000, "Test Product", "Food", "Brand"));
+
+        assertEquals(testService.getTotalPrice(), 1000);
     }
 
     @Test
     void getCartSize() {
+        CartServiceIMPL testService = new CartServiceIMPL();
+
+        testService.addProduct(new ProductEntity(1000, "Test Product", "Food", "Brand"));
+
+        assertEquals(testService.getCartSize(), 1);
     }
 }
